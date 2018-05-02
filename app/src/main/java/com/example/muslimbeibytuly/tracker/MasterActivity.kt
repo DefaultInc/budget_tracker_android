5078c4c8529c8d7859e2cedf92fd8ca4f85f38ec
NurlashKO <zh.nurlan96@gmail.com>
2018-05-02 09:36:18 +0600

    added expandable fab buttons, as discussed.

--- app/src/main/java/com/example/muslimbeibytuly/tracker/MasterActivity.kt
+++ app/src/main/java/com/example/muslimbeibytuly/tracker/MasterActivity.kt
@@ -6,13 +6,17 @@ import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
 import android.support.annotation.Nullable
+import android.support.design.widget.FloatingActionButton
 import android.support.design.widget.NavigationView
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import android.widget.ListView
 import com.example.muslimbeibytuly.tracker.adapters.TransactionsListViewAdapter
 import com.example.muslimbeibytuly.tracker.models.NewTransaction
@@ -23,11 +27,33 @@ import com.jjoe64.graphview.series.DataPoint
 import com.jjoe64.graphview.series.LineGraphSeries
 import kotlinx.android.synthetic.main.activity_master.*
 import kotlinx.android.synthetic.main.app_bar_master.*
+import com.wangjie.rapidfloatingactionbutton.util.RFABTextUtil.dip2px
+import com.wangjie.rapidfloatingactionbutton.contentimpl.labellist.RFACLabelItem
+import com.wangjie.rapidfloatingactionbutton.contentimpl.labellist.RapidFloatingActionContentLabelList
+import com.wangjie.rapidfloatingactionbutton.RapidFloatingActionHelper
+import com.wangjie.rapidfloatingactionbutton.util.RFABTextUtil.dip2px
+import com.wangjie.rapidfloatingactionbutton.RapidFloatingActionButton
+import com.wangjie.rapidfloatingactionbutton.RapidFloatingActionLayout
 
 
-class MasterActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+
+
+
+
+class MasterActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener, View.OnClickListener {
 
     private var mTransactionViewModel: TransactionViewModel? = null
+
+    // Fabs
+    private var isFabOpen = false;
+    private var fab: FloatingActionButton? = null
+    private var fabWrite: FloatingActionButton? = null
+    private var fabCamera: FloatingActionButton? = null
+    private var fab_open: Animation? = null
+    private var fab_close: Animation? = null
+    private var rotate_forward: Animation? = null
+    private var rotate_backward: Animation? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -49,6 +75,11 @@ class MasterActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelec
                 )
         )
 
+        series.color =  Color.RED
+        series.backgroundColor = Color.rgb(255, 130, 149)
+        series.isDrawBackground = true
+        graph.addSeries(series)
+
         mTransactionViewModel?.getAllTransactions()?.observe(this, Observer<List<NewTransaction>>() {
             val adapter = TransactionsListViewAdapter(this, it as ArrayList<NewTransaction>)
             val transactionsListView = findViewById<ListView>(R.id.transactionsListView)
@@ -57,13 +88,6 @@ class MasterActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelec
         })
 
 
-        /**
-         * camera image capture activity
-         * */
-        fab.setOnClickListener { view ->
-            val intent = Intent(this, CameraActivity()::class.java)
-            startActivity(intent)
-        }
         val toggle = ActionBarDrawerToggle(
                 this,
                 drawer_layout,
@@ -74,6 +98,7 @@ class MasterActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelec
         toggle.syncState()
 
         nav_view.setNavigationItemSelectedListener(this)
+        buildFabs()
     }
 
     override fun onBackPressed() {
@@ -126,4 +151,54 @@ class MasterActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelec
         drawer_layout.closeDrawer(GravityCompat.START)
         return true
     }
+
+    override fun onClick(v: View) {
+        val id = v.id
+        when (id) {
+            R.id.fab ->
+                animateFAB()
+            R.id.fab_write->
+                Log.d("Raj", "Fab 1")
+            R.id.fab_camera-> {
+                /**
+                 * camera image capture activity
+                 **/
+                val intent = Intent(this, CameraActivity()::class.java)
+                startActivity(intent)
+            }
+        }
+    }
+
+    fun animateFAB(){
+        if(isFabOpen){
+            fab?.startAnimation(rotate_backward);
+            fabWrite?.startAnimation(fab_close);
+            fabCamera?.startAnimation(fab_close);
+            fabWrite?.setClickable(false);
+            fabCamera?.setClickable(false);
+            isFabOpen = false;
+        } else {
+            fab?.startAnimation(rotate_forward);
+            fabWrite?.startAnimation(fab_open);
+            fabCamera?.startAnimation(fab_open);
+            fabWrite?.setClickable(true);
+            fabCamera?.setClickable(true);
+            isFabOpen = true;
+        }
+    }
+
+
+    fun buildFabs() {
+        fab = findViewById(R.id.fab) as FloatingActionButton
+        fabWrite = findViewById(R.id.fab_write) as FloatingActionButton
+        fabCamera = findViewById(R.id.fab_camera) as FloatingActionButton
+        fab_open = AnimationUtils.loadAnimation(this.applicationContext, R.anim.fab_open);
+        fab_close = AnimationUtils.loadAnimation(this.applicationContext,R.anim.fab_close);
+        rotate_forward = AnimationUtils.loadAnimation(this.applicationContext,R.anim.rotate_forward);
+        rotate_backward = AnimationUtils.loadAnimation(this.applicationContext,R.anim.rotate_backward);
+        fab?.setOnClickListener(this)
+        fabWrite?.setOnClickListener(this)
+        fabCamera?.setOnClickListener(this)
+    }
+
 }
